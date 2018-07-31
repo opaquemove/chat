d633cdcf3a599e32ed0ee6ae35270acca03174d5
USER <user@USER-no-MacBook-Air.local>
2018-07-31 21:03:25 +0900

    commot 31.jul.2018-3

--- app.js
+++ app.js
@@ -54,7 +54,10 @@ server.on( 'listening', () => {
   console.log( 'listening on 3000' );
 });
 
-server.listen( 3000 );
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+// server.listen( 3000 );
+server.listen( port, ipaddress );
 
 /*
 setInterval( function() {
