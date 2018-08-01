a44bfe3f4e391e8615255a3a92498a72d0b8af98
USER <user@USER-no-MacBook-Air.local>
2018-08-01 20:20:54 +0900

    1.Aug.2018-6

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 const http = require('http');
 const server = http.createServer( (req, res ) => {
