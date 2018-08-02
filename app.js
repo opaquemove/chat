b3b6c668392e90d028579c5e8fb2990122a7efbb
1139970619478 <1139970619478@nsl.ad.nec.co.jp>
2018-08-02 12:42:42 +0900

    2.Aug.2018-4

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var express = require('express');
 var app     = express();
-//var server  = require('http').Server(app);
+var server  = require('http').Server(app);
 
-//app.use('/', express.static( __dirname + '/public' ));
+app.use('/', express.static( __dirname + '/public' ));
 
+/*
 const http = require('http');
 const server = http.createServer( (req, res ) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain' );
   res.end( 'Hello World' );
 });
+*/
 
 server.on( 'listening', () => {
   console.log( 'listening on ' + port );
