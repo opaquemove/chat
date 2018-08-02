403250a0251da9280faa372152bfc00e441fca15
1139970619478 <1139970619478@nsl.ad.nec.co.jp>
2018-08-02 10:06:58 +0900

    2.Aug.2018-3

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var express = require('express');
+var app     = express();
+//var server  = require('http').Server(app);
+
+//app.use('/', express.static( __dirname + '/public' ));
 
 const http = require('http');
 const server = http.createServer( (req, res ) => {
